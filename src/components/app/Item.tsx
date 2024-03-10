495ce33d1809edae4a24a3f055cdf5c12dc06e53
mohd-arz <mohammedarsh75@gmail.com>
2024-03-10 18:14:02 +0530

    feat:Image PopUp

--- src/components/app/Item.tsx
+++ src/components/app/Item.tsx
@@ -1,5 +1,11 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 
 interface Itemtype{
   menu:string,
@@ -29,21 +35,43 @@ function Item({
       <div className='food-container m-4 sm:m-6 md:m-8'>
         {foodArr.map((num)=>{
           return(
-            <div key={num} className='food-item flex pb-6 gap-6' >
-              <div className='w-[80%]'>
-              <LazyLoadImage
-                alt={dishes[0].name+' image'}
-                effect="blur"
-                src={dishes[0].img}
-                className='rounded-2xl xl:max-w-[80%] min-w-[100px]'
-              />
-              </div>
-              <div className='flex flex-col'>
-                <h2 className='text-base' style={{fontFamily:'Quicksand'}}>{dishes[0].name}</h2>
-                <p className='text-xs pt-2'>{dishes[0].detail}</p>
-                <p className='mt-auto'>AED {dishes[0].price}</p>
-              </div>
-            </div>
+            <Dialog>
+              <DialogTrigger className='focus:outline-none' tabIndex={-1}>
+                <div key={num} className='food-item flex pb-6 gap-6' >
+                  <div className='w-[80%]'>
+                  <LazyLoadImage
+                    alt={dishes[0].name+' image'}
+                    effect="blur"
+                    src={dishes[0].img}
+                    className='rounded-2xl xl:max-w-[80%] min-w-[100px]'
+                  />
+                  </div>
+                  <div className='flex flex-col'>
+                    <h2 className='text-base' style={{fontFamily:'Quicksand'}}>{dishes[0].name}</h2>
+                    <p className='text-xs pt-2'>{dishes[0].detail}</p>
+                    <p className='mt-auto'>AED {dishes[0].price}</p>
+                  </div>
+                </div>
+              </DialogTrigger>
+              <DialogContent>
+                <div key={num} className='food-item-modal flex flex-col pb-6 gap-6' >
+                  <div className=''>
+                    <LazyLoadImage
+                      alt={dishes[0].name+' image'}
+                      effect="blur"
+                      src={dishes[0].img}
+                    />
+                  </div>
+                  <div className='flex flex-col m-4'>
+                    <div className='flex justify-between'>
+                      <h2 className='text-base' style={{fontFamily:'Quicksand'}}>{dishes[0].name}</h2>
+                      <p className='mt-auto'>AED {dishes[0].price}</p>
+                    </div>
+                      <p className='text-xs pt-2'>{dishes[0].detail}</p>
+                  </div>
+                </div>
+            </DialogContent>
+            </Dialog>
           )
         })}
       </div>
